9416502d0a35ee45fc2fa43bd684656f95e463b0
jammacn <jeff@jamma.cn>
2017-03-14 13:04:14 +0800

    using jm-server

--- config/index.js
+++ config/index.js
@@ -1,42 +1,31 @@
+require('log4js').configure(__dirname + '/log4js.json');
 var config = {
     development: {
         debug: true,
         port: 20170,
-        prefix:'/oms',
         mq: 'redis://localhost',
         sdk: 'http://test.gzleidi.cn:82',
-        ms: [
-            {
-                type: 'ws'
-            },
-            {
-                type: 'http'
+        modules: {
+            oms: {
+                module: process.cwd() + '/lib'
             }
-        ]
+        }
     },
     production: {
         port: 20170,
-        prefix:'/oms',
         mq: 'redis://redis.db',
         sdk: 'http://api.web',
-        ms: [
-            {
-                type: 'ws'
-            },
-            {
-                type: 'http'
+        modules: {
+            oms: {
+                module: process.cwd() + '/lib'
             }
-        ]
+        }
     }
 };
 
 var env = process.env.NODE_ENV||'development';
 config = config[env]||config['development'];
 config.env = env;
-
-['debug', 'port', 'prefix', 'trustProxy', 'mq', 'sdk'].forEach(function(key) {
-    process.env[key] && (config[key]=process.env[key]);
-});
 
 module.exports = config;
 
