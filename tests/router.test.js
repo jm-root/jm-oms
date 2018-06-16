6020b60db763872e53fed7ff796c273e9776529b
jamma <jeff@jamma.cn>
2018-06-17 00:57:46 +0800

    test

--- tests/router.test.js
+++ tests/router.test.js
@@ -5,13 +5,19 @@ beforeAll(async () => {
   router = $.router()
 })
 
-test('nav', async () => {
-  let doc = await router.request({
-    uri: '/nav',
-    headers: {
-      acl_user: '596d5cb3baeeaf00203de4ec'
+test('nav', (done) => {
+  router.request(
+    {
+      uri: '/nav',
+      type: 'get',
+      headers: {
+        acl_user: '596d5cb3baeeaf00203de4ec'
+      }
+    },
+    (err, doc) => {
+      console.log(doc)
+      expect(doc.ret).toBeTruthy()
+      done()
     }
-  })
-  console.log(doc)
-  expect(doc).toBeTruthy()
+  )
 })
